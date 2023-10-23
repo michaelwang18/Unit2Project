f7424059b34d04231e382979fa5975b38a2b56b8
BT_2N2_32 <sample@bths.edu>
2023-10-23 14:00:45 -0400

    Rewrote format, needs Slope Display

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,9 +1,7 @@
-import javax.sound.sampled.Line;
+
 import java.util.Scanner;
 
 public class LinearEquationLogic {
-
-
     //Variables
     boolean finished = false;
     String cord1;
@@ -24,13 +22,7 @@ public class LinearEquationLogic {
     public void start(){
         while (!finished){
             welcome();
-            //Calculate and reprint all info
-            System.out.println("The Two Points Are: " + cord1 + " And " + cord2);
-            System.out.println("The Equation Of The Line Between The Two Points Is: " + "placehold");
-            System.out.println("The Slope Of The Equation Is: " + calc.findSlope(x1,y1,x2,y2));
-            System.out.println("The y-Intercept Of The Equation Is: " + calc.yIntercept(x1,y1,x2,y2));
-            System.out.println("The Distance Between The 2 Coordinates Is: " + calc.distanceBetween(x1,y1,x2,y2));
-
+            System.out.println(response());
         }
         //"main" so to say
 
@@ -41,32 +33,32 @@ public class LinearEquationLogic {
     public void welcome(){
         System.out.println("Welcome to Linear Equation Calc \nWhat is your first coordinate");
         cord1 = scan.nextLine();
-        int x1 = Integer.parseInt(cord1.substring(1,cord1.indexOf(",")));
-        int y1 = Integer.parseInt(cord1.substring(cord1.indexOf(" ") + 1,cord1.indexOf(")")));
+         x1 = Integer.parseInt(cord1.substring(1,cord1.indexOf(",")));
+         y1 = Integer.parseInt(cord1.substring(cord1.indexOf(" ") + 1,cord1.indexOf(")")));
         System.out.println("What is your second coordinate?");
         cord2 = scan.nextLine();
-        int x2 = Integer.parseInt(cord2.substring(1,cord2.indexOf(",")));
-        int y2 = Integer.parseInt(cord2.substring(cord2.indexOf(" ") + 1,cord2.indexOf(")")));
+         x2 = Integer.parseInt(cord2.substring(1,cord2.indexOf(",")));
+         y2 = Integer.parseInt(cord2.substring(cord2.indexOf(" ") + 1,cord2.indexOf(")")));
+
+
 
     }
 
+    public String response() {
+        String resp = "";
+        if (x1 == x2) {
+            return "These Points Are On The Horizontal Line X = " + x1;
+        } else if (y1 == y2) {
+            return "These Points Are On The Vertical Line Y = " + y1;
+        }
+        if (x1 != x2 && y1 != y2) {
+            resp += "The Two Points Are: " + cord1 + " And " + cord2 + "\n";
+            resp += "The Equation Of The Line Between The Two Points Is: " + "placehold" + "\n";
+            resp += "The Slope Of The Equation Is: " + calc.findSlope(x1, y1, x2, y2) + "\n";
+            resp += "The y-Intercept Of The Equation Is: " + calc.yIntercept(x1, y1, x2, y2) + "\n";
+            resp += "The Distance Between The 2 Coordinates Is: " + calc.distanceBetween(x1, y1, x2, y2) + "\n";
+        }
+        return resp;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
